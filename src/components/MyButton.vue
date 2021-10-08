098d097c133d5a61912a76a6f24b6ac790b33257
wujian <1982057859@qq.com>
2021-10-08 19:19:15 +0800

    first

---
+++ src/components/MyButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>我爱你赵丽颖</h2>
+    <button @click="count++">爱了{{ count }}</button>
+  </div>
+</template>
+<script type="text/javascript" >
+export default{
+    name:'',
+    data(){
+        return{
+            count:0
+        }
+    }
+}
+</script>
